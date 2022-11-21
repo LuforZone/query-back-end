4725a69230036624b935406d56b1cdc2c1ee15a1
lufor <1056252297@163.com>
2022-11-22 00:03:35 +0800

    config.default

--- app/middleware/middlewareFuckCors.ts
+++ app/middleware/middlewareFuckCors.ts
@@ -3,7 +3,7 @@ const fuckCors = async (ctx: { set: (arg0: string, arg1: string) => void; }, nex
   // allow header get,postm,put,delete,patch,head,options
   ctx.set('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   ctx.set('Access-Control-Allow-Credentials', 'true');
-  ctx.set('Access-Control-Allow-Origin', '96.43.94.20:3000');
+  ctx.set('Access-Control-Allow-Origin', 'http://96.43.94.20:3000');
   ctx.set('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
 };
 
